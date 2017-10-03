624679fe6ddc4bc82afdf0e457ae7760856fb2d4
Colin J. Mills <colin.j.mills96@gmail.com>
2017-10-03 19:58:29 -0400

    Add .gitignore

---
+++ MAD_A01/MAD_A01Tests/MAD_A01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MAD_A01Tests.swift
+//  MAD_A01Tests
+//
+//  Created by Student on 2017-10-03.
+//  Copyright © 2017 SetWESNET. All rights reserved.
+//
+
+import XCTest
+@testable import MAD_A01
+
+class MAD_A01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
